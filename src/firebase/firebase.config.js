3c53f491420eba515508e0b48b0543c3deca1cb6
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-25 19:08:17 +0600

    Install react-router, react-bootstrap and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQmHovoLP1zbuKBNSMghIzvdZH3ecrbho",
+  authDomain: "web-academy-a9013.firebaseapp.com",
+  projectId: "web-academy-a9013",
+  storageBucket: "web-academy-a9013.appspot.com",
+  messagingSenderId: "528613156829",
+  appId: "1:528613156829:web:10ca33663a0974cba63834",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
