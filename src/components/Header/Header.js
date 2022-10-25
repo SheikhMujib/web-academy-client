6b050e7d2150e5c2809db78a6a192b7435e8fea1
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-25 21:28:17 +0600

    Website's name displayed on title and website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Web Academy</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link></Link>
+            <NavLink></NavLink>
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </div>
     );
 };
